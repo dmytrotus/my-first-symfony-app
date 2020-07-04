ebba9c7755749a31cd57768ccb02da90746e29b8
Dmytro Tus <dmytrotus@gmail.com>
2020-07-04 16:12:19 +0200

    make todo model and migration

---
+++ src/Entity/ToDo.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ToDoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ToDoRepository::class)
+ */
+class ToDo
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @ORM\Column(type="text", length=256)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $is_done;
+}
